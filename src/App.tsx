81030503834a49a02b54294f750cf41632e3c1d9
MrJackQiao <qc1994qc099@gmail.com>
2023-02-10 10:35:40 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload test.
+                automatically reload test1.
             </p>
         </div>
     );
